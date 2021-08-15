b94ecdea2299bfe35abac8bf57791a14b367c038
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-08-15 15:08:09 +0900

    Create 2475.cpp

---
+++ BOJ/2475.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double UniqueNumber1, UniqueNumber2, UniqueNumber3, UniqueNumber4, UniqueNumber5;
+
+    cin >> UniqueNumber1 >> UniqueNumber2 >> UniqueNumber3 >> UniqueNumber4 >> UniqueNumber5;
+
+    int UniqueNumber6 = (pow(UniqueNumber1, 2) + pow(UniqueNumber2, 2) + pow(UniqueNumber3, 2)  + pow(UniqueNumber4, 2) + pow(UniqueNumber5, 2));
+
+    cout << UniqueNumber6 % 10 << endl;
+}

0c810923e1a17051900abb46ce890e5db8d13bfb
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-08-20 11:23:40 +0900

    Create 2530.cpp

---
+++ BOJ/2530.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int h, m, s, D, Dh, Dm, Ds;
+    cin >> h >> m >> s >> D;
+    
+    int mr, sr;
+
+    Dh = D / 3600;
+    Dm = (D % 3600) / 60;
+    Ds = (D % 3600) % 60;
+
+    int sec = s + Ds;
+    int sadd = sec / 60;
+    s = sec % 60;
+
+    int min = m + Dm + sadd;
+    int madd = min / 60;
+    m = min % 60;
+
+    int hour = h + Dh + madd;
+    h = hour % 24;
+
+    cout << h << " " << m << " " << s << "\n";
+
+    return 0;
+}

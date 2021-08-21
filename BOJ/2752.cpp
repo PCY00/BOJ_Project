7a1b408302ec1faff8c27513d97a100c08301883
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-08-21 13:25:59 +0900

    Create 2752.cpp

---
+++ BOJ/2752.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void Print(vector<int> &a) {
+    for (int i = 0; i < 3; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    vector<int> a(3);
+
+    for (int i = 0; i < 3; i++) {
+        cin >> a[i];
+    }
+
+    sort(a.begin(), a.end(), less<int>());
+
+    Print(a);
+}

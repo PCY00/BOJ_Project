6cc1f410ea479170fd8856a7a3ad2fbc48f4797b
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-08-23 10:22:50 +0900

    Create 2444.cpp

---
+++ BOJ/2444.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+
+    cin >> N;
+
+    for (int k = 0; k < N; k++) {
+        for (int i = 1; i < N - k; i++) {
+            cout << " ";
+        }
+        for (int j = 0; j < 2 * k + 1; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (int k = 1; k < N; k++) {
+        for (int j = 0; j < k; j++) {
+            cout << " ";
+        }
+        for (int i = 0; i < 2 * (N - k) - 1; i++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}

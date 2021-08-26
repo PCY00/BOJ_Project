fbc646795e840af14adfa70bb5a2ef4c6ac07444
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-08-26 15:44:42 +0900

    Create 2754.cpp

---
+++ BOJ/2754.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string score;
+	double sum = 0.0;
+
+	cin >> score;
+
+	for (int i = 0; i < score.length(); i++) {
+		switch (score[i]) {
+		case 'A':
+			sum += 4;
+			break;
+		case 'B':
+			sum += 3;
+			break;
+		case 'C':
+			sum += 2;
+			break;
+		case 'D':
+			sum += 1;
+			break;
+		case 'F':
+			break;
+		case '+':
+			sum += 0.3;
+			break;
+		case '-':
+			sum -= 0.3;
+			break;
+		case '0':
+			break;
+		}
+	}
+	cout << fixed;
+	cout.precision(1);
+	cout << sum << "\n";
+	return 0;
+}

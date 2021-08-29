0fa5a1a028577970f9df63d40b07cf97dc3097e9
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-08-29 17:44:14 +0900

    Create 2592.cpp

---
+++ BOJ/2592.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int arr[10] = { 0, };
+	int sum = 0;
+	int count[10] = { 0, };
+
+	for (int i = 0; i < 10; i++) {
+		cin >> arr[i];
+		sum += arr[i];
+	}
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			if (arr[i] == arr[j]) {
+				count[i] += 1;
+			}
+		}
+	}
+	int num = 0, a = 0;
+	for (int i = 0; i < 10; i++) {
+		if (num < count[i]) {
+			num = count[i];
+			a = i;
+		}
+	}
+	sum = sum / 10;  //평균
+	cout << sum << endl;
+	cout << arr[a] << endl;
+
+	return 0;
+}

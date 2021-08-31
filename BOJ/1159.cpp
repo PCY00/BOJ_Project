e1fc484d88cf24af295bf3d3b8500aaac970ad54
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-08-31 17:13:32 +0900

    Create 1159.cpp

---
+++ BOJ/1159.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string str;
+	int N;
+	char a;
+	int Alphabet[26] = {0, };
+	int count = 26;
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		cin >> str;
+		Alphabet[str[0] - 'a']++;
+		str.clear();
+	}
+	for (int i = 0; i < 26; i++) {
+		if (Alphabet[i] > 4) {
+			a = i + 'a';
+			cout << a;
+		}
+		else {
+			count--;
+		}
+	}
+	if (count == 0) {
+		cout << "PREDAJA";
+	}
+	return 0;
+}

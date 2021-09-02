520ffcfb5ba6b2a86580f24dbe68afab33ae3188
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-09-02 20:21:23 +0900

    Create 1264.cpp

---
+++ BOJ/1264.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string str;
+	char arr[10] = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+	int count = 0;
+
+	while (1) {
+		getline(cin, str);
+
+		if (str == "#") {
+			break;
+		}
+		for (int i = 0; i < str.length(); i++) {
+			for (int j = 0; j < 10; j++) {
+				if (str[i] == arr[j]) {
+					count++;
+				}
+			}
+		}
+		cout << count << endl;
+		count = 0;
+	}
+	return 0;
+}

07021dbc44e650f4eb1c98752ec9ba07fad75b60
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-09-03 10:01:38 +0900

    Create 1668.cpp

---
+++ BOJ/1668.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N, n;
+	int Lcount = 0, Rcount = 0;
+	cin >> N;
+
+	int max = 0;
+	
+	int arr[50] = { 0, };
+
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+	for (int i = 0; i < N; i++) {
+		if (max < arr[i]) {
+			max = arr[i];
+			Lcount++;
+		}
+	}
+	max = 0;
+	n = N;
+	for (int i = 0; i < (N / 2); i++) {
+		int temp = arr[i];
+		arr[i] = arr[n - 1];
+		arr[n- 1] = temp;
+		n--;
+	}
+	for (int i = 0; i < N; i++) {
+		if (max < arr[i]) {
+			max = arr[i];
+			Rcount++;
+		}
+	}
+	cout << Lcount << endl;
+	cout << Rcount << endl;
+	return 0;
+}

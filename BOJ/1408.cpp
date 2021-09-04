5d0a07849982e8d56d11ac6aa86695ea6a34dd6a
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-09-04 15:02:03 +0900

    Create 1408.cpp

---
+++ BOJ/1408.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() { 
+	int Nhour, Nmin, Nsec, Ehour, Emin, Esec;
+
+	int timeH, timeM, timeS;
+
+	scanf_s("%d:%d:%d", &Nhour, &Nmin, &Nsec);
+	scanf_s("%d:%d:%d", &Ehour, &Emin, &Esec);
+	
+	timeS =  Esec - Nsec;
+	timeH = Ehour - Nhour;
+	timeM = Emin - Nmin;
+	
+	if (timeS < 0) {
+		timeS += 60;
+		timeM--;
+	}
+	if (timeM < 0) {
+		timeM += 60;
+		timeH--;
+	}
+	if (timeH < 0) {
+		timeH += 24;
+	}
+	printf("%02d:%02d:%02d", timeH, timeM, timeS);
+
+	return 0;
+}

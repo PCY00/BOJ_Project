8b6b25f3573c7aea39c4cda8d437359d84703df1
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-09-05 13:00:26 +0900

    Create 10162.cpp

---
+++ BOJ/10162.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int A = 5, B = 1, C = 10;
+	int T, t;
+	int Ccount = 0, Bcount = 0, Acount = 0;
+
+	cin >> T;
+	
+	t = T % 60;
+	T = T / 60;
+	if (T == 0) {
+		Ccount = t / C;
+	}
+	if (T >= 5) {
+		Acount = T / A;
+	}
+	T = T % A;
+	if (T < 5) {
+		Bcount = T / B;
+	}
+	Ccount = t / C;
+	if (t % C != 0) {
+		cout << "-1";
+	}
+	else {
+		cout << Acount << " " << Bcount << " " << Ccount << endl;
+	}
+	return 0;
+}

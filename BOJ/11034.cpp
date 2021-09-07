0fa8d2d6a1d385f4a77a57ccee9de73a1c23cbca
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-09-07 09:04:06 +0900

    Create 11034.cpp

---
+++ BOJ/11034.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream> 
+using namespace std;
+int main() {
+	int A, B, C;
+	int max = 0;
+	while (cin >> A >> B >> C) {
+		if ((B - A) > (C - B)) {
+			max = (B - A) - 1;
+		}
+		else {
+			max = (C - B) - 1;
+		}
+		cout << max << endl;
+		if (cin.eof()) break;
+	}
+	return 0;
+}

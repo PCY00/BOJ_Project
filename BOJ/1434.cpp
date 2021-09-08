d7157683f7705d6a61b1d2e53b712d62cff82243
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-09-08 17:57:49 +0900

    Create 1434.cpp

---
+++ BOJ/1434.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+
+	int* box = new int[n];
+	int* book = new int[m];
+
+	for (int i = 0; i < n; i++) {
+		cin >> box[i];
+	}
+	for (int i = 0; i < m; i++) {
+		cin >> book[i];
+	}
+	int remain = 0;
+	
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (box[i] >= book[j]) {
+				box [i] = box[i] - book[j];
+				book[j] = 0;
+			}
+		}
+		remain += box[i];
+	}
+	cout << remain;
+	return 0;
+}

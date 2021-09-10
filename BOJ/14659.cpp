88373af2a209e4997c3f77478364fc07c84cb0c0
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-09-11 08:08:23 +0900

    Create 14659.cpp

---
+++ BOJ/14659.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	int* count = new int[N];
+	int* height = new int[N];
+
+	for (int i = 0; i < N; i++) {
+		cin >> height[i];
+	}
+	int max = 0;
+	int save = 0;
+	for (int i = 0; i < N; i++) {
+		count[i] = 0;
+		if (max < height[i]) {
+			max = height[i];
+			save = 0;
+		}
+		else if (max > height[i]) {
+			save += 1;
+			count[i] = save;
+		}
+	}
+	save = 0;
+	for (int i = 0; i < N; i++) {
+		if (save < count[i]) {
+			save = count[i];
+		}
+	}
+	cout << save;
+	delete[] count;
+	delete[] height;
+	return 0;
+}

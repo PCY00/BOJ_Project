945a8fd1089c533538cfbe4f6e64be53d9e20f4b
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-09-13 16:07:08 +0900

    Create 14487.cpp

---
+++ BOJ/14487.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using  namespace std;
+
+int main() {
+	int n, result = 0, max = 0;
+	cin >> n;
+	int* town = new int[n];
+	
+	for (int i = 0; i < n; i++) {
+		cin >> town[i];
+		result += town[i];
+		if (max <= town[i]) {
+			max = town[i];
+		}
+	}
+	result -= max;
+
+	cout << result << endl;
+	delete[] town;
+	return 0;
+}

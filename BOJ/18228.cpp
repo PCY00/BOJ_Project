10331f09a808953e56474b0fe8224a5287214c23
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-09-15 14:19:17 +0900

    Create 18228.cpp

---
+++ BOJ/18228.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int water[200000];
+int pos;
+
+int main() {
+	int n;
+	int power = 1000000000;
+	int result = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> water[i];
+		if (water[i] == -1) {
+			pos = i;
+		}
+	}
+	for (int i = 0; i < pos; i++) {
+		if (water[i] < power) {
+			power = water[i];
+		}
+	}
+	result += power;
+	power = 1000000000;
+	for (int i = pos + 1; i < n; i++) {
+		if (water[i] < power) {
+			power = water[i];
+		}
+	}
+	result += power;
+
+	cout << result;
+	return 0;
+}

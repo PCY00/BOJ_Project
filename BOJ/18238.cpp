169808fa564691b3870a51a0aae7ccf07a6bd494
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-09-16 13:52:53 +0900

    Create 18238.cpp

---
+++ BOJ/18238.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string str;
+	cin >> str;
+
+	char box = 'A';
+	int min = 0;
+	int count = 0, count2 = 0, result = 0;
+
+	for (int i = 0; i < str.length(); i++) {
+		count = abs(str[i] - box);
+		count2 = 26 - count;
+		if (count < count2) {
+			min += count;
+		}
+		else {
+			min += count2;
+		}
+		box = str[i];
+	}
+	cout << min;
+	return 0;
+}

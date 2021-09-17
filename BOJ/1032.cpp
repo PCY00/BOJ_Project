4aa9934670ea0e873f7a6d66fbe22cfa2c147f7e
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-09-17 16:57:47 +0900

    Create 1032.cpp

---
+++ BOJ/1032.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	string* s = new string[N];
+	for (int i = 0; i < N; i++) {
+		cin >> s[i];
+	}
+	int L = s[0].length();
+	char r[50];
+	for (int i = 0; i < L; i++) {
+		r[i] = s[0][i];
+	}
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < L; j++) {
+			if (r[j] == s[i][j]) {
+				r[j] = s[i][j];
+			}
+			else {
+				r[j] = '?';
+			}
+		}
+	}
+	for (int i = 0; i < L; i++) {
+		cout << r[i];
+	}
+	delete[] s;
+	return 0;
+}

b199c314d2ae4c533f27c172a92a5b3f85ddf59e
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-09-21 14:33:54 +0900

    Create 1453.cpp

---
+++ BOJ/1453.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool check[100] = { false, };
+
+int main() {
+	int N, count = 0;
+	cin >> N;
+	int* sit = new int[N];
+	for (int i = 0; i < N; i++) {
+		cin >> sit[i];
+		if (check[sit[i]] == true) {
+			count++;
+		}
+		check[sit[i]] = true;
+	}
+	cout << count;
+	delete[] sit;
+	return 0;
+}

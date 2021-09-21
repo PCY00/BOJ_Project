9130002d5fecdb2d697fdaf6f15f5cf918cb6ea3
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-09-21 14:34:23 +0900

    Create 2693.cpp

---
+++ BOJ/2693.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n, count = 0;
+	int arr[10], result[1000];
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int i = 0; i < 10; i++) {
+			cin >> arr[i];
+		}
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				if (arr[i] > arr[j]) {
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		count = arr[2];
+		cout << count << endl;
+	}
+	return 0;
+}

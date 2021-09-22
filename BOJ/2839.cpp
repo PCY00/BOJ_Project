fe9175e98bdcd562e2f00944806b77e18cffe361
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-09-22 10:22:58 +0900

    Create 2839.cpp

---
+++ BOJ/2839.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N, Fc = 0, Tc = 0;
+	int result = 0;
+	cin >> N;
+	Fc = N / 5;
+	N = N % 5;
+	Tc = N / 3;
+	if (N % 3 == 0){
+		result = Fc + Tc;
+	}
+	else {
+		while (1) {
+			if (Fc != 0) {
+				Fc--;
+				N = N + 5;
+			}
+			Tc = N / 3;
+			if (N % 3 == 0) {
+				result = Fc + Tc;
+				break;
+			}
+			if(Fc == 0){
+				result = -1;
+				break;
+			}
+		}
+	}
+	cout << result << endl;
+	return 0;
+}

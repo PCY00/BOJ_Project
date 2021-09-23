62f49952a98c81cbcb39772a13f5aba329d8dd07
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-09-23 22:50:20 +0900

    Create 2702-유클리드_호제법.cpp

---
+++ BOJ/2702-유클리드_호제법.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b);
+int lcm(int a, int b);
+
+int main() {
+	int N;
+	cin >> N;
+	int N1, N2;
+	for (int i = 0; i < N; i++) {
+		cin >> N1 >> N2;
+		cout << lcm(N1, N2) << " " << gcd(N1, N2) << endl;
+	}
+	return 0;
+}
+
+int gcd(int a, int b) {
+	int c;
+	while (b != 0) {
+		c = a % b;
+		a = b;
+		b = c;
+	}
+	return a;
+}
+
+int lcm(int a, int b) {
+	return (a * b) / gcd(a, b);
+}

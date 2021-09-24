88e8954c553de1f49672911fa2e5c5fe50ca83f5
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-09-24 12:38:20 +0900

    Create 1110.cpp

---
+++ BOJ/1110.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N, count = 1;
+	int first = 0, save = 0, sum = 0;
+	cin >> N;
+	first = N;
+	while (1) {
+		if (N < 10) {
+			N = (N * 10) + N;
+		}
+		else {
+			save = N / 10;
+			N = N % 10;
+			sum = save + N;
+			N = (N * 10) + (sum % 10);
+			save = 0;
+		}
+		if (N == first) {
+			break;
+		}
+		count++;
+	}
+	cout << count << endl;
+	return 0;
+}

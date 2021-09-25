198de5967a1606230df371b00fd9c5c7a6ae3fe8
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-09-25 10:36:51 +0900

    Create 2609.cpp

---
+++ BOJ/2609.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b);
+void lcd(int i, int j);
+
+int main() {
+	int num1, num2;
+	cin >> num1 >> num2;
+	cout << gcd(num1, num2) << endl;
+	lcd(num1, num2);
+	return 0;
+}
+
+int gcd(int a, int b) {
+	int c;
+	while (b != 0) {
+		c = a % b;
+		a = b;
+		b = c;
+	}
+	return a;
+}
+void lcd(int i, int j) {
+	cout << (i * j) / gcd(i, j) << endl;
+}

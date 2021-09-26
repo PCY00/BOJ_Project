eb65958cccc4e2e03e0b98761daf1c1846160e46
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-09-26 23:38:27 +0900

    Create 3447.cpp

---
+++ BOJ/3447.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+#include <regex>
+
+using namespace std;
+
+int main() {
+	string str;
+	while (getline(cin, str)) {
+		while (str.find("BUG") != -1) {
+			str = regex_replace(str, regex("BUG"), "");
+		}
+		cout << str << endl;
+	}
+}

562aa5267b65d0fc28da716945fb4b0516824642
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-09-27 11:40:25 +0900

    Create 9093.cpp

---
+++ BOJ/9093.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	cin.ignore();
+	string str;
+
+	for (int i = 0; i < N; i++) {
+		getline(cin, str);
+		for (int j = 0; j < str.length(); j++) {
+			if (str[j] == ' ') {
+				for (int k = j - 1; str[k] != ' ' && k >= 0; k--) {
+					cout << str[k];
+				}
+				cout << " ";
+			}
+		}
+		for (int j = str.length() - 1; str[j] != ' ' && j >= 0; j--) {
+			cout << str[j]; 
+		}
+		cout << " \n";
+	}
+	return 0;
+}

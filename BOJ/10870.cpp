1bb319886619ea18d1fc701c15ea263868bd8c91
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-09-29 12:54:21 +0900

    Create 10870.cpp

---
+++ BOJ/10870.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int arr[1001];
+
+int DP(int F) {
+	if (F == 0) return 0;
+	if (F == 1) return 1;
+	if (arr[F] != 0) return arr[F];
+	return arr[F] = DP(F - 1) + DP(F - 2);
+}
+
+int main() {
+	int n;
+	cin >> n;
+	cout << DP(n);
+	return 0;
+}

0c71a71b2bd2dade1785e4eae3ba02d6ba58b5e0
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-10-02 23:00:36 +0900

    Create 1681.cpp

---
+++ BOJ/1681.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int N;
+	string L;
+	cin >> N >> L;
+	int a = 0;
+	for (int i = 0; i < N; i++) {
+		while (to_string(a++).find(L) != string::npos);
+	}
+	cout << a << endl;
+	return 0;
+}

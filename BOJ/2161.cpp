237e0d9ecf1103390ee5aebe21ea7938b7468ed1
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-10-05 09:55:10 +0900

    Create 2161.cpp

---
+++ BOJ/2161.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <deque>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	deque <int> d;
+	int N;
+	cin >> N;
+	int arr[1000] = { 0, };
+	for (int i = 1; i <= N; i++) {
+		d.push_back(i);
+	}
+	for (int i = 1; i <= N; i++) {
+		arr[i] = d.front();
+		d.pop_front();
+		if (d.empty()) {
+			break;
+		}
+		d.push_back(d.front());
+		d.pop_front();
+	}
+	for (int i = 1; i <= N; i++) {
+		cout << arr[i] << " ";
+	}
+	return 0;
+}

4115b6fd344aebba138d1d5b570f9fcb9683c10d
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-10-07 19:35:01 +0900

    Create 12605.cpp

---
+++ BOJ/12605.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	string str;
+	stringstream str1;
+	stack <string> s;
+	cin.ignore();
+	for (int i = 0; i < N; i++) {
+		getline(cin, str);
+		str1.str(str);
+		while (str1 >> str) {
+			s.push(str);
+		}
+		str1.clear();
+		cout << "Case #" << i + 1 << ": ";
+		for (int i = s.size() - 1; i >= 0; i--) {
+			cout << s.top() << " ";
+			s.pop();
+		}
+		cout << endl;
+	}
+	return 0;
+}

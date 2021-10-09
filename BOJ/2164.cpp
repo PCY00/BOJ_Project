703cca56f7ca4b5e7e71e435ddf7a9fa373b5bc1
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-10-09 17:04:37 +0900

    Create 2164.cpp

---
+++ BOJ/2164.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	queue <int> q;
+	int save = 0;
+	for (int i = 1; i <= n; i++) {
+		q.push(i);
+	}
+	for (int i = n; i > 1; i--) {
+		q.pop();
+		q.push(q.front());
+		q.pop();
+	}
+	cout << q.front() << endl;
+	return 0;
+}

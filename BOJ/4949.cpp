eaa37c80cc4ed570dabc440f2b74afe7645acc16
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-10-10 15:57:00 +0900

    Create 4949.cpp

---
+++ BOJ/4949.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int main() {
+	stack <char> s;
+	string str;
+	while (getline(cin, str)) {
+		bool check = true;
+		if (str == ".") {
+			break;
+		}
+		for (int i = 0; i < str.length(); i++) {
+			if (str[i] == '.') {
+				break;
+			}
+			else if (str[i] == '(' || str[i] == '[') {
+				s.push(str[i]);
+			}
+			if (str[i] == ')') {
+				if (!s.empty() && s.top() == '(') {
+					s.pop();
+				}
+				else {
+					check = false;
+					break;
+				}
+			}
+			else if (str[i] == ']') {
+				if (!s.empty() && s.top() == '[') {
+					s.pop();
+				}
+				else {
+					check = false;
+					break;
+				}
+			}
+		}
+		if (s.empty() && check) {
+			cout << "yes" << endl;
+		}
+		else {
+			cout << "no" << endl;
+		}
+		for (;;) {
+			if (s.size() == 0) {
+				break;
+			}
+			s.pop();
+		}
+	}
+	return 0;
+}

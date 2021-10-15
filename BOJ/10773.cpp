d0d602ba12764ca859a619de6553f756cafeeada
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-10-15 12:59:19 +0900

    Create 10773.cpp

---
+++ BOJ/10773.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+	int K, num;
+	cin >> K;
+	int size = 0;
+	stack <int> s;
+	for (int i = 0; i < K; i++) {
+		cin >> num;
+		if (num != 0) {
+			s.push(num);
+		}
+		else if (num == 0 && !s.empty()) {
+			s.pop();
+		}
+	}
+	size = s.size();
+	int sum = 0;
+	for (int i = 0; i < size; i++) {
+		sum += s.top();
+		s.pop();
+	}
+	cout << sum << endl;
+	return 0;
+}

95fb0fa0b229f9c7c7e542c63e2b436a130ee9f9
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-10-16 09:59:59 +0900

    Create 9655.cpp

---
+++ BOJ/9655.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	if (N % 2 == 0) {
+		cout << "CK" << endl;
+	}
+	else {
+		cout << "SK" << endl;
+	}
+	return 0;
+}

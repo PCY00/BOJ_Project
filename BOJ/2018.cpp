891d44ab62253a60105c449f9a271f5c00ecf803
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-10-19 21:04:02 +0900

    Create 2018.cpp

---
+++ BOJ/2018.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	int N;
+	int count = 0, sum = 1;
+	scanf_s("%d", &N);
+	int start = 1, end = 2;
+	while (start <= N) {
+		sum += end;
+		if (sum == N) {
+			count++;
+			start++;
+			end = start;
+			sum = 0;
+		}
+		else if (sum > N) {
+			start++;
+			sum = 0;
+			end = start;
+		}
+		else {
+			end++;
+		}
+	}
+	printf("%d", count);
+	return 0;
+}

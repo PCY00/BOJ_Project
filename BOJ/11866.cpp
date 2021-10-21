d7bdac26efdb779de9cf4afebfd27599a7537b8b
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-10-21 19:31:10 +0900

    Create 11866.cpp

---
+++ BOJ/11866.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+	int k, n;
+	cin >> k >> n;
+	queue <int> q;
+	for (int i = 1; i <= k; i++) {
+		q.push(i);
+	}
+	cout << "<";
+	while (1) {
+		if (q.empty()) {
+			break;
+		}
+		for (int i = 1; i <= n - 1; i++) {
+			q.push(q.front());
+			q.pop();
+		}
+		cout << q.front();
+		q.pop();
+		if (!q.empty()) {
+			cout << ",";
+		}
+	}
+	cout << ">";
+	return 0;
+}

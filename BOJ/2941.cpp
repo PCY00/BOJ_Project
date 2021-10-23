bc7e940ccf8563458db0daf4489f7d064144833c
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-10-23 21:13:52 +0900

    Create 2941.cpp

---
+++ BOJ/2941.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string str;
+	string str1 = "1";
+	int save = 0;
+	int count = 0;
+	cin >> str;
+	string s[8] = { "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
+	for (int i = 0; i < 8; i++) {
+		for (;;) {
+			if (str.find(s[i]) != string::npos) {
+				save = str.find(s[i]);
+				str.replace(save, s[i].length(), str1);
+			}
+			else if (str.find(s[i]) == string::npos) {
+				break;
+			}
+		}
+	}
+	for (int i = 0; i < str.length(); i++) {
+		count++;
+	}
+	cout << count << endl;
+	return 0;
+}

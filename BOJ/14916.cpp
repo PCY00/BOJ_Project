929c93cad49a037660a89cea2bd900eaca3ef877
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-10-29 19:34:38 +0900

    Create 14916.cpp

---
+++ BOJ/14916.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int n, cnt = 0;
+    cin >> n;
+    while (!(n % 5 == 0 || n <= 0)) {
+        n -= 2;
+        cnt++;
+    }
+    if (n < 0) {
+        cout << "-1";
+        return 0;
+    }
+    cout << n / 5 + cnt;
+}

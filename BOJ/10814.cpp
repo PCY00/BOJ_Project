bf22da6ab6203d501f409d14b040c681e029e7af
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-10-30 16:46:03 +0900

    Create 10814.cpp

---
+++ BOJ/10814.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+struct per {
+	int age;
+	int num;
+	string name;
+};
+
+bool cmp(const per& a1, const per& a2) {
+	if (a1.age < a2.age) {
+		return true;
+	}
+	else if (a1.age == a2.age) {
+		return a1.num < a2.num;
+	}
+	else {
+		return false;
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n;
+	cin >> n;
+	per* s = new per[n];
+	for (int i = 0; i < n; i++) {
+		cin >> s[i].age >> s[i].name;
+		s[i].num = i;
+	}
+	sort(s, s + n, cmp);
+	for (int i = 0; i < n; i++) {
+		cout << s[i].age << " " << s[i].name << '\n';
+	}
+	delete[] s;
+	return 0;
+}

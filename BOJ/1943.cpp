7f6e76b8155bf697fc0f923557a2c61a437c692a
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-11-04 15:48:32 +0900

    Create 1943.cpp

---
+++ BOJ/1943.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b) {
+	int r = a % b;
+	if (r == 0) return b;
+	else return gcd(b, r);
+}
+
+int main() {
+	int n, a, b, save = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a >> b;
+		save = gcd(a, b);
+		a /= save; b /= save;
+		cout << save * a * b << endl;
+	}
+	return 0;
+}

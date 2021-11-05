85aeaac092d52ccf951c846c3ced9fb1766b029f
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-11-05 17:05:44 +0900

    Create 1292.cpp

---
+++ BOJ/1292.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int arr[1000000] = { 0, };
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int a, b, c = 1;
+	cin >> a >> b;
+	int sum = 0;
+	for (int i = 1; i <= b; i++) {
+		for (int j = 1; j <= i; j++) {
+			arr[c] = i;
+			c++;
+		}
+	}
+	for (int i = a; i <= b; i++) {
+		sum += arr[i];
+	}
+	cout << sum;
+	return 0;
+}

d77d211c5ce71adcefa023ed5290892c294595ab
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-11-08 19:46:43 +0900

    Create 1978.cpp

---
+++ BOJ/1978.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool ck(int p) {
+	int cut = 0;
+	for (int i = 1; i <= p; i++) {
+		if (p % i == 0) {
+			cut++;
+		}
+	}
+	if (cut == 2) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n, num;
+	int cut = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> num;
+		if (ck(num) == true) {
+			cut++;
+		}
+	}
+	cout << cut << '\n';
+	return 0;
+}

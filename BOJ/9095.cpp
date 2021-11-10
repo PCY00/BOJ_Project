b3dd612bf04cbc891c7a8cb16728b0ed5b592569
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-11-10 20:43:02 +0900

    Create 9095.cpp

---
+++ BOJ/9095.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
+	int dp[12] = { 0, };
+	int t, n;
+	cin >> t;
+
+	dp[1] = 1;
+	dp[2] = 2;
+	dp[3] = 4;
+
+	for (int i = 0; i < t; i++) {
+		cin >> n;
+		for (int j = 4; j <= n; j++) {
+			dp[j] = dp[j - 3] + dp[j - 2] + dp[j - 1];
+		}
+
+		cout << dp[n] << endl;
+	}
+	return 0;
+}

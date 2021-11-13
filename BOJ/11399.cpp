9f94a4487cf884c7ff39cce46544aeb1cc569ddb
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-11-13 20:25:06 +0900

    Create 11399.cpp

---
+++ BOJ/11399.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int n, save = 0;
+	cin >> n;
+	int* arr = new int[n];
+	int* sum = new int[n];
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	sort(arr, arr + n);
+	for (int i = 0; i < n; i++) {
+		save += arr[i];
+		sum[i] = save;
+	}
+	save = 0;
+	for (int i = 0; i < n; i++) {
+		save += sum[i];
+	}
+	cout << save;
+	delete[] arr;
+	delete[] sum;
+	return 0;
+}

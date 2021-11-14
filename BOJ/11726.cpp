31354c8151d16274a978ceba234d2c73162c8ac0
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-11-14 20:07:44 +0900

    Create 11726.cpp

---
+++ BOJ/11726.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n;
+	int dp[1001] = { 0, };
+	cin >> n;
+	dp[1] = 1;
+	dp[2] = 2;
+	dp[3] = 3;
+	for (int i = 4; i <= n; i++) {
+		dp[i] = dp[i - 2] + dp[i - 1];
+		dp[i] %= 10007;
+	}
+	cout << dp[n] << '\n';
+	return 0;
+}

b16f4ea9a4b93dd9258500ca5186989bc0d7885f
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-11-15 19:11:36 +0900

    Create 11727.cpp

---
+++ BOJ/11727.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int ti[1001];
+int dp(int n);
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n;
+	cin >> n;
+	cout << dp(n) << '\n';
+	return 0;
+}
+
+int dp(int n) {
+	if (n == 1) return 1;
+	if (n == 2) return 3;
+	if (ti[n] != 0) return ti[n];
+	return ti[n] = (dp(n - 1) + 2 * dp(n - 2)) % 10007;
+}

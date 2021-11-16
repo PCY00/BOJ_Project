bf43f9ca81bb9a37037511e7cd30294e5e3a133d
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-11-16 19:32:42 +0900

    Create 1874.cpp

---
+++ BOJ/1874.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n, num;
+	int a = 1;
+	stack <int> s;
+	string str = "";
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> num;
+		if (num >= a) {
+			for (;;) {
+				if (num + 1 == a) break;
+				s.push(a++);
+				str += "+\n";
+			}
+			s.pop();
+			str += "-\n";
+		}
+		else {
+			if (s.top() == num) {
+				s.pop();
+				str += "-\n";
+			}
+			else {
+				cout << "NO";
+				return 0;
+			}
+		}
+	}
+	cout << str;
+	return 0;
+}

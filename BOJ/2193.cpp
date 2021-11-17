8d4e3eb5ef8b21e73a5ed0c3a41e6333bbabf019
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-11-17 14:19:29 +0900

    Create 2193.cpp

---
+++ BOJ/2193.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+long long arr[1001];
+long long dp(int n);
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n; // 자리수
+	cin >> n;
+	cout << dp(n);
+	return 0;
+}
+
+long long dp(int n) {
+	if (n == 0) return 0;
+	if (n == 1) return 1;
+	if (n == 2) return 1;
+	if (arr[n] != 0) return arr[n];
+	return arr[n] = dp(n - 1) + dp(n - 2);
+}

96149ed34ec47347f10926861f89826ee062920a
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-11-19 17:38:57 +0900

    Create 9461.cpp

---
+++ BOJ/9461.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+long long d[1000001] = { 0, };
+long long dp(int n);
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n, num;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> num;
+		cout << dp(num) << '\n';
+	}
+	return 0;
+}
+long long dp(int n) {
+	if (n == 1) return 1;
+	if (n == 2) return 1;
+	if (n == 3) return 1;
+	if (n == 4) return 2;
+	if (n == 5) return 2;
+	if (n == 6) return 3;
+	if (n == 7) return 4;
+	if (n == 8) return 5;
+	if (d[n] != 0) return d[n];
+	else {
+		return d[n] = dp(n - 1) + dp(n - 5);
+	}
+	return 0;
+}

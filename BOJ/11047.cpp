19e407c9fb3b1b28c87b0e784075562e87d263fd
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-11-21 10:53:53 +0900

    Create 11047.cpp

---
+++ BOJ/11047.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int arr[11];
+int cnt[11];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n, k;
+	cin >> n >> k;
+	int save = k;
+	for (int i = n; i >= 1; i--) {
+		cin >> arr[i];
+	}
+	for (int j = 1; j <= n; j++) {
+		for (int i = j; i <= n; i++) {
+			if (save / arr[i] != 0) {
+				cnt[j] = cnt[j] + (save / arr[i]);
+				save %= arr[i];
+			}
+		}
+		save = k;
+	}
+	sort(cnt, cnt + n);
+	cout << cnt[1] << '\n';
+	return 0;
+}

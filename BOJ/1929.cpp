2f165f64431544fc8c0003b9ee84a34c5d95bec6
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-11-22 22:00:25 +0900

    Create 1929.cpp

---
+++ BOJ/1929.cpp
@@ -0,0 +1,48 @@
+﻿#include <stdio.h>
+
+
+
+int arr[1000001] = { 0, };
+
+
+
+int main(void) {
+
+	int n, m, i, j;
+
+	arr[0] = 1; arr[1] = 1;
+
+	scanf("%d %d", &n, &m);
+
+	for (i = 2; i <= m; i++) {
+
+		if (arr[i] == 1) {
+
+			continue;
+
+		}
+		else {
+
+			for (j = 2; i * j <= m; j++) {
+
+				arr[i * j] = 1;
+
+			}
+
+		}
+
+	}
+
+	for (i = n; i <= m; i++) {
+
+		if (arr[i] == 0) {
+
+			printf("%d\n", i);
+
+		}
+
+	}
+
+	return 0;
+
+}

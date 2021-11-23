f73b727dda16db2588a8e9f36169e693f2232555
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-11-23 20:40:50 +0900

    Create 2579.cpp

---
+++ BOJ/2579.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int arr[301] = { 0, };
+int dp[301] = { 0, };
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> arr[i];
+	}
+	dp[1] = arr[1];
+	dp[2] = arr[1] + arr[2];
+	dp[3] = max(arr[1] + arr[3], arr[2] + arr[3]);
+	for (int i = 4; i <= n; i++) {
+		dp[i] = max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i]);
+	}
+	cout << dp[n] << '\n';
+	return 0;
+}

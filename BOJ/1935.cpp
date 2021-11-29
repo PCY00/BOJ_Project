6dc25c848344baa6ea73b54998b48fb2d106e971
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-11-30 07:36:08 +0900

    Create 1935.cpp

---
+++ BOJ/1935.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cout << fixed;
+	cout.precision(2);
+	int Case, j = 0;
+	string str;
+	stack <double> s;
+	double save = 0.0, sum = 0.0;
+	cin >> Case >> str;
+	int* num = new int[Case];
+	for (int i = 0; i < Case; i++) {
+		cin >> num[i];
+	}
+	for (int i = 0; i < str.length(); i++) {
+		if (str[i] <= 'Z' && str[i] >= 'A') {
+			s.push(num[str[i] - 'A']);
+		}
+		else {
+			if (str[i] == '*') {
+				save = s.top();
+				s.pop();
+				sum = save * s.top();
+				s.pop();
+				s.push(sum);
+			}
+			else if (str[i] == '+') {
+				save = s.top();
+				s.pop();
+				sum = save + s.top();
+				s.pop();
+				s.push(sum);
+			}
+			else if (str[i] == '/') {
+				save = s.top();
+				s.pop();
+				sum = s.top() / save;
+				s.pop();
+				s.push(sum);
+			}
+			else if (str[i] == '-') {
+				save = s.top();
+				s.pop();
+				sum = s.top() - save;
+				s.pop();
+				s.push(sum);
+			}
+		}
+	}
+	cout << s.top() << '\n';
+	delete[] num;
+	return 0;
+}

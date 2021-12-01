794aefacd983eae9c96090ac93076ee55a5ff57d
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-12-01 19:37:23 +0900

    Create 2748.cpp

---
+++ BOJ/2748.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+long long d[100001];
+
+long long dp(int n);
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n;
+	cin >> n;
+	cout << dp(n) << '\n';
+	return 0;
+}
+
+long long dp(int n) {
+	if (n == 0) return 0;
+	if (n == 1) return 1;
+	if (n == 1) return 1;
+	if (d[n] != 0) return d[n];
+	else {
+		return d[n] = dp(n - 1) + dp(n - 2);
+	}
+}

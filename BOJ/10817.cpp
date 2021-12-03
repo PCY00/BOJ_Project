475cc5df7fdc72d24a03c289232058bd8c956d91
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-12-03 19:37:51 +0900

    Create 10817.cpp

---
+++ BOJ/10817.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int arr[3];
+	for (int i = 0; i < 3; i++) {
+		cin >> arr[i];
+	}
+	for (int i = 0; i < 3; i++) {
+		for (int j = i; j < 3; j++) {
+			if (arr[i] > arr[j]) {
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+	cout << arr[1] << '\n';
+	return 0;
+}

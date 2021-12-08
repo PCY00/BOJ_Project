38179d828e15c05f0f2da79e4a74076b4aff0a1d
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-12-08 18:34:05 +0900

    Create 1427.cpp

---
+++ BOJ/1427.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+bool comp(char a, char b);
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	string str;
+	cin >> str;
+	char* arr = new char[str.length() + 1];
+	for (int i = 0; i < str.length(); i++) {
+		arr[i] = str[i];
+	}
+	sort(arr, arr + str.length(), comp);
+	for (int i = 0; i < str.length(); i++) {
+		cout << arr[i];
+	}
+	delete[] arr;
+	return 0;
+}
+
+bool comp(char a, char b) {
+	if (a <= b) {
+		return false;
+	}
+	else {
+		return true;
+	}
+}

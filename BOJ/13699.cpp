92f577a4f1512c46cdfdf958530d5fa7c392962b
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-12-09 19:32:09 +0900

    Create 13699.cpp

---
+++ BOJ/13699.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+long long arr[36];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int num;
+	cin >> num;
+	arr[0] = 1;
+	for (int i = 1; i <= num; i++) {
+		for (int j = 1; j <= i; j++) {
+			arr[i] += arr[j - 1] * arr[i - j];
+		}
+	}
+	cout << arr[num] << '\n';
+	return 0;
+}

0932a7494bb7512fc48a1093d2881ac174404260
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-12-18 21:56:23 +0900

    Create 5576.cpp

---
+++ BOJ/5576.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int arr[10];
+    int brr[10];
+    for (int i = 0; i < 10; i++) {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < 10; i++) {
+        cin >> brr[i];
+    }
+    sort(arr, arr + 10);
+    sort(brr, brr + 10);
+    int sum = 0;
+    int summ = 0;
+    for (int i = 9; i >= 7; i--) {
+        sum += arr[i];
+        summ += brr[i];
+    }
+    cout << sum << " " << summ;
+    return 0;
+}

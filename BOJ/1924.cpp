4e3192529f566953f6d07aa98d2a6c4901392a2f
PCY00 <46348321+PCY00@users.noreply.github.com>
2021-12-21 19:53:17 +0900

    Create 1924.cpp

---
+++ BOJ/1924.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int re(int x, int y);
+string str[7] = {  "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+
+int main() {
+	int x, y, save;  // x == month, y == day, save == 일수 저장.
+	cin >> x >> y;
+	save = re(x, y) % 7;
+	cout << str[save] << "\n";
+	return 0;
+}
+
+int re(int x, int y) {
+	int sum = 0;
+	for (int i = 1; i <	x; i++) {
+		if (i == 4 || i == 6 || i == 9 || i == 11) {
+			sum += 30;
+		}
+		else if (i == 2) {
+			sum += 28;
+		}
+		else {
+			sum += 31;
+		}
+	}
+	sum += y;
+	return sum;
+}

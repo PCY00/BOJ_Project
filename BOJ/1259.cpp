966db2f1216ac6a8385af8978c249a743a98c5b0
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-01-03 20:46:40 +0900

    Create 1259.cpp

---
+++ BOJ/1259.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	string num;
+	while (1) {
+		int ck = 0;
+		cin >> num;
+		if (num == "0") break;
+		for (int i = 0; i < num.length() / 2; i++) {
+			if (num[i] != num[num.length() - (i + 1)]) {
+				cout << "no" << '\n';
+				ck = 1;
+				break;
+			}
+		}
+		if (ck == 0) {
+			cout << "yes" << '\n';
+		}
+	}
+	return 0;
+}

fc5f8e5eb6b295389056d08c568f7adcda4cf65e
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-01-05 15:25:29 +0900

    Create 10988.cpp

---
+++ BOJ/10988.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	string str;
+	int cnt = 0;
+	cin >> str;
+	for (int i = 0; i < (str.length() / 2); i++) {
+		if (str[i] != str[str.length() - (i + 1)]) {
+			cnt++;
+		}
+	}
+	if (cnt != 0) {
+		cout << "0" << '\n';
+	}
+	else {
+		cout << "1" << '\n';
+	}
+	return 0;
+}

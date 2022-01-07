c4e1b0975cfe678c2616e7453b4c6804917b4f68
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-01-07 17:12:30 +0900

    Create 2563.cpp

---
+++ BOJ/2563.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int white[101][101] = { 0, };
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int Case, x, y;
+	int sum = 0;
+	cin >> Case;
+	for (int i = 0; i < Case; i++) {
+		cin >> x >> y;
+		for (int j = x; j < x + 10; j++) {
+			for (int k = y; k < y + 10; k++) {
+				white[k][j] = 1;
+			}
+		}
+	}
+	for (int i = 0; i <= 100; i++) {
+		for (int j = 0; j <= 100; j++) {
+			if (white[i][j] == 1) sum += 1;
+		}
+	}
+	cout << sum << '\n';
+	return 0;
+}

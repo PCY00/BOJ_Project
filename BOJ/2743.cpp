82f72bba6be8f7c867dfee6097dca5f477c9811d
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-01-10 20:20:29 +0900

    Create 2743.cpp

---
+++ BOJ/2743.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int mystrlen(const char *str);
+
+int main() {
+	char arr[101] = "";
+	int i = 0;
+	while (!cin.eof()) {
+		cin >> arr[i];
+		i++;
+	}
+	cout << mystrlen(arr) << '\n';
+	return 0;
+}
+
+int mystrlen(const char *str) {
+	int cnt = 0;
+	for (cnt = 0; str[cnt]; cnt++); // str[cnt]는 종료문자('\n')이 나올 때까지
+	return cnt;
+}

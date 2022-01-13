51b470d1df302ac6693b2030c6b1b63ceea47d87
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-01-13 20:19:31 +0900

    Create 10872.cpp

---
+++ BOJ/10872.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int factorial(int n);
+
+int main() {
+	int n;
+	cin >> n;
+	cout << factorial(n);
+	return 0;
+}
+
+int factorial(int n) {
+	int m = 1;
+	for (int i = 1; i <= n; i++) {
+		m *= i;
+	}
+	return m;
+}

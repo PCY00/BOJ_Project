4ce9466753c3b378ef3043ec0d427be2a9882309
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-01-14 19:40:17 +0900

    Create 2292.cpp

---
+++ BOJ/2292.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int cnt(int n);
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n;
+	cin >> n;
+	cout << cnt(n) << '\n';
+	return 0;
+}
+
+int cnt(int n) {
+	int re = 1, save = 0;
+	int sum = 1;
+	while (1) {
+		if (n == 1) return re;
+		else {
+			save += 6;
+			sum += save;
+			re++;
+			if (n - sum <= 0) return re;
+		}
+	}
+}

3ea67262a65701d65fc76370284367daf075b6f3
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-01-15 13:57:50 +0900

    Create 10039.cpp

---
+++ BOJ/10039.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int re(int n);
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int num = 5;
+	cout << re(num) << '\n';
+	return 0;
+}
+
+int re(int n) {
+	int sum = 0;
+	int save;
+	for (int i = 0; i < n; i++) {
+		cin >> save;
+		if (save < 40) {
+			sum += 40;
+		}
+		else {
+			sum += save;
+		}
+	}
+	sum /= n;
+	return sum;
+}

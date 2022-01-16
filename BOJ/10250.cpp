6a68abd65ea757911ad654c52e0042009d2d63d8
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-01-16 10:58:27 +0900

    Create 10250.cpp

---
+++ BOJ/10250.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int init(int h, int w, int n);
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int t, h, w, n;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> h >> w >> n;
+		cout << init(h, w, n) << '\n';
+	}
+	return 0;
+}
+
+int init(int h, int w, int n) {
+	int save = 1;
+	int ho, fl;
+	ho = (n / h) + 1;
+	fl = n % h;
+	if (fl == 0) {
+		fl = h;
+		ho -= 1;
+	}
+	save = (fl * 100) + ho;
+	return save;
+}

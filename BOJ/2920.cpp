2343143cccb44d912e7f94ac7860ce6abc983796
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-01-25 18:16:50 +0900

    Create 2920.cpp

---
+++ BOJ/2920.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int num[9];
+	int ck = 0;
+	for (int i = 1; i <= 8; i++) {
+		cin >> num[i];
+	}
+	for (int i = 1; i < 8; i++) {
+		if (num[i] < num[i + 1]) {
+			ck++;
+		}
+		else {
+			ck--;
+		}
+	}
+	if (ck == 7) {
+		cout << "ascending" << '\n';
+	}
+	else if (ck == -7) {
+		cout << "descending" << '\n';
+	}
+	else {
+		cout << "mixed" << '\n';
+	}
+	return 0;
+}

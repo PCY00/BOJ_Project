237669a32b8ca0ca3221980843c2c63010140be5
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-01-27 13:57:20 +0900

    Create 2747.cpp

---
+++ BOJ/2747.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int re[46] = { 0, };
+int fibo(int n);
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int a;
+	cin >> a;
+	cout << fibo(a) << '\n';
+	return 0;
+}
+
+int fibo(int n) {
+	int sum = 0;
+	re[0] = 0;
+	re[1] = 1;
+	re[2] = 1;
+	if (n >= 2) {
+		for (int i = 3; i <= n; i++) {
+			re[i] = re[i - 1] + re[i - 2];
+		}
+	}
+	return re[n];
+}

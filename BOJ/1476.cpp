c84976ea5336ff3691b6c14845d0a95b6d45d206
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-01-29 17:21:27 +0900

    .

---
+++ BOJ/1476.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int arr[3] = { 0, };
+	int brr[3] = { 1, 1, 1 };
+	int cnt = 1;
+	for (int i = 0; i < 3; i++) {
+		cin >> arr[i];
+	}
+	while (1) {
+		if (arr[0] == brr[0] && arr[1] == brr[1] && arr[2] == brr[2]) break;
+		if (brr[0] == 15) {
+			brr[0] = 0;
+		}
+		if (brr[1] == 28) {
+			brr[1] = 0;
+		}
+		if (brr[2] == 19) {
+			brr[2] = 0;
+		}
+		brr[0]++;
+		brr[1]++;
+		brr[2]++;
+		cnt++;
+	}
+	cout << cnt << '\n';
+	return 0;
+}

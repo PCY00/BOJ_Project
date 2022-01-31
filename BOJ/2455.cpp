52060a254cef42d2ed0fe0af5b23122dbefeb32c
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-02-01 08:52:14 +0900

    Create 2455.cpp

---
+++ BOJ/2455.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#define NUM 4
+
+using namespace std;
+
+int main() {
+	int down, get;
+	int max = 0, sum = 0;
+	for (int i = 0; i < NUM; i++) {
+		cin >> down >> get;
+		sum -= down;
+		if (max < sum) max = sum;
+		sum += get;
+		if (max < sum) max = sum;
+	}
+	cout << max << '\n';
+	return 0;
+}

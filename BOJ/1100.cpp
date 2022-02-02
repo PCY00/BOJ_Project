763f0b40c5369e09d63c380fc527251adbcbeb6a
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-02-03 08:28:09 +0900

    Create 1100.cpp

---
+++ BOJ/1100.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#define max 8
+
+using namespace std;
+
+char arr[8][8];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int ck = 0;
+	for (int i = 0; i < max; i++) {
+		for (int j = 0; j < max; j++) {
+			cin >> arr[i][j];
+			if (i % 2 == 0 && j % 2 == 0) {
+				if (arr[i][j] == 'F') {
+					ck++;
+				}
+			}
+			else if (i % 2 != 0 && j % 2 != 0) {
+				if (arr[i][j] == 'F') {
+					ck++;
+				}
+			}
+		}
+	}
+	cout << ck << '\n';
+	return 0;
+}

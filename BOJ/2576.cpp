9a372e70f8b0c93d7396e308a88323d709eaeeaa
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-02-04 08:43:32 +0900

    Create 2576.cpp

---
+++ BOJ/2576.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#define num 7
+
+using namespace std;
+
+int main() {
+	int ck;
+	int sum = 0, save = 100;
+	for (int i = 0; i < num; i++) {
+		cin >> ck;
+		if (ck % 2 != 0) {
+			sum += ck;
+			if (save >= ck) {
+				save = ck;
+			}
+		}
+	}
+	if (sum != 0 && save != 100) {
+		cout << sum << '\n' << save << '\n';
+	}
+	else {
+		cout << "-1" << '\n';
+	}
+	return 0;
+}

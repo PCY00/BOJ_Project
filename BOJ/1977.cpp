cd82d4021d49d0232e26de9baaf279a350a4e4cf
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-02-05 08:39:43 +0900

    Create 1977.cpp

---
+++ BOJ/1977.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	int N, M;
+	int sum = 0;
+	int save = 10001;
+	cin >> N >> M;
+	for (int i = 1; i <= 100; i++) {
+		if (i * i >= N && i * i <= M) {
+			sum += (i * i);
+			if (save >= i * i) {
+				save = i * i;
+			}
+		}
+	}
+	if (save != 10001 && sum != 0) {
+		cout << sum << '\n' << save << '\n';
+	}
+	else {
+		cout << "-1" << '\n';
+	}
+	return 0;
+}
+

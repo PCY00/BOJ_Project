fed689854b72ae6ec34aef33df8857fdf20d5596
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-02-06 11:36:57 +0900

    Create 2902.cpp

---
+++ BOJ/2902.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	string str;
+	cin >> str;
+	for (int i = 0; i < str.length(); i++) {
+		if (str[i] <= 'Z' && str[i] >= 'A') {
+			cout << str[i];
+		}
+	}
+	return 0;
+}

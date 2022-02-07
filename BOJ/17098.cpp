ac29e76e58db8043a4888a5ee33dd23031e3651a
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-02-07 10:43:21 +0900

    Create 17098.cpp

---
+++ BOJ/17098.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#define num 5
+
+using namespace std;
+
+int main() {
+	string* str = new string[num];
+	unsigned int max = 0;
+	for (int i = 0; i < num; i++) {
+		cin >> str[i];
+		if (max < str[i].length()) {
+			max = str[i].length();
+		}
+	}
+	for (unsigned int i = 0; i < max; i++) {
+		for (unsigned int j = 0; j < num; j++) {
+			if (i < str[j].length()) {
+				cout << str[j][i];
+			}
+		}
+	}
+	delete[] str;
+	return 0;
+}

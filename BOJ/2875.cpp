35d6fef2709c6c0a2b008c2b25d4ddbbf566f0cd
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-02-08 20:10:02 +0900

    Create 2875.cpp

---
+++ BOJ/2875.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N, M, K;
+	int cnt = 0;
+	cin >> N >> M >> K;
+	int sum = 0;
+	sum = (N + M - K) / 3;
+	if (sum > N / 2) {
+		cout << N / 2 << '\n';
+	}
+	else if (sum > M) {
+		cout << M << '\n';
+	}
+	else if (N == 0 || M == 0) {
+		cout << "0" << '\n';
+	}
+	else {
+		cout << sum << '\n';
+	}
+	return 0;
+}

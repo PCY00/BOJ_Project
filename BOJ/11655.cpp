33b0891a89439d03602f5c740d749fcb0d86a663
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-02-09 09:11:08 +0900

    Create 11655.cpp

---
+++ BOJ/11655.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string str;
+	getline(cin, str);
+	for (int i = 0; i < str.length(); i++) {
+		if (str[i] <= 'z' && str[i] >= 'a') {
+			str[i] = (((str[i] - 'a') + 13) % 26) + 'a';
+		}
+		else if (str[i] <= 'Z' && str[i] >= 'A') {
+			str[i] = (((str[i] - 'A') + 13) % 26) + 'A';
+		}
+	}
+	cout << str << '\n';
+	return 0;
+}

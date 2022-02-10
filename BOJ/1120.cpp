adf5c57bac716b6baf6120522bd4ce4e59909a59
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-02-10 12:09:55 +0900

    Create 1120.cpp

---
+++ BOJ/1120.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int ck1 = 0;
+	int min = 50;
+	string str1, str2;
+	cin >> str1 >> str2;
+	for (int i = 0; i <= str2.length() - str1.length(); i++) {
+		for (int j = 0; j < str1.length(); j++) {
+			if (str1[j] != str2[j + i]) {
+				ck1++;
+			}
+		}
+		if (ck1 <= min) {
+			min = ck1;
+		}
+		ck1 = 0;
+	}
+	cout << min << '\n';
+	return 0;
+}

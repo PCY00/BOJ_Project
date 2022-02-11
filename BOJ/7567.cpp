5580e5182e2b70506fbf8dc5c428a060cc130915
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-02-11 10:00:30 +0900

    Create 7567.cpp

---
+++ BOJ/7567.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string str;
+	cin >> str;
+	int ck = 10;
+	for (int i = 1; i < str.length(); i++) {
+		if (str[i] == str[i - 1]) {
+			ck += 5;
+		}
+		else {
+			ck += 10;
+		}
+	}
+	cout << ck << '\n';
+	return 0;
+}

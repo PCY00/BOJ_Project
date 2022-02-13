e61deffbe0353514e6e37b0e34b2e835804ae8b2
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-02-13 11:31:54 +0900

    Create 2010.cpp

---
+++ BOJ/2010.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int N, cnt;
+	int sum = 0;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> cnt;
+		sum += cnt;
+	}
+	cout << sum - (N - 1)<< '\n';
+	return 0;
+}

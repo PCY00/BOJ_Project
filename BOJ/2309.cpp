820f951bca726d176395879b6c45c1a8f46ca3d6
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-02-23 10:32:36 +0900

    Create 2309.cpp

---
+++ BOJ/2309.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <algorithm>
+#define num 9
+
+using namespace std;
+
+int main() {
+	int arr[num];
+	int sum = 0;
+	int save1 = 0;
+	int save2 = 0;
+	for (int i = 0; i < num; i++) {
+		cin >> arr[i];
+		sum += arr[i];
+	}
+	sort(arr, arr + num);
+	while (1) {
+		int re = sum;
+		for (int i = 0; i < num; i++) {
+			for (int j = i + 1; j < num; j++) {
+				re = re - (arr[i] + arr[j]);
+				if (re == 100) {
+					save1 = i;
+					save2 = j;
+					break;
+				}
+				re = sum;
+			}
+		}
+		break;
+	}
+	for (int i = 0; i < num; i++) {
+		if (save1 != i && save2 != i) {
+			cout << arr[i] << '\n';
+		}
+	}
+	return 0;
+}

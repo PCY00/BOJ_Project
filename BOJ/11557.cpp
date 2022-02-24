66483f34d58ea03cb66715d01e12b1808fa6d703
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-02-24 10:52:45 +0900

    Create 11557.cpp

---
+++ BOJ/11557.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+typedef struct {
+	string str;
+	int K;
+}S;
+
+int main() {
+	int max = 0, result = 0;
+	int Case, N;
+	cin >> Case;
+	for (int i = 0; i < Case; i++) {
+		cin >> N;
+		S* s = new S[N];
+		for (int j = 0; j < N; j++) {
+			cin >> s[j].str >> s[j].K;
+			if (max < s[j].K) {
+				max = s[j].K;
+				result = j;
+			}
+		}
+		cout << s[result].str << '\n';
+		delete[] s;
+	}
+	return 0;
+}

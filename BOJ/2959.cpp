7de752f932c2845c1ac0cc892f166f0965ae593f
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-03-01 13:29:04 +0900

    Create 2959.cpp

---
+++ BOJ/2959.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int arr[4];
+	for (int i = 0; i < 4; i++) {
+		cin >> arr[i];
+	}
+	sort(arr, arr + 4);
+	int result = 0;
+	result = arr[0] * arr[2];
+	cout << result;
+	return 0;
+}

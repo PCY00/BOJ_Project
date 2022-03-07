4d5033f839e1a0a734720c19ee41e8ae98eb17f9
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-03-07 17:12:06 +0900

    Create 17219.cpp

---
+++ BOJ/17219.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n, m;
+	cin >> n >> m;
+	string str, str2;
+	map<string, string> a;
+	for (int i = 0; i < n; i++) {
+		cin >> str >> str2;
+		a[str] = str2;
+	}
+	for (int i = 0; i < m; i++) {
+		cin >> str;
+		cout << a[str] << '\n';
+	}
+	return 0;
+}

3c904f6c40f962f4e1a067b195dd4283a1f0d98f
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-03-10 18:01:03 +0900

    Create 2217.cpp

---
+++ BOJ/2217.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int n, sum = 0;
+	cin >> n;
+	int* arr = new int[n];
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	sort(arr, arr + n, greater<>());
+	for (int i = 0; i < n; i++) {
+		arr[i] *= i + 1;
+		if (sum < arr[i]) {
+			sum = arr[i];
+		}
+	}
+	cout << sum << '\n';
+	delete[] arr;
+	return 0;
+}

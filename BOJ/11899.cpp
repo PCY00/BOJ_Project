af11ec938b0121d114619e234a45cb04d016b766
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-03-11 19:21:36 +0900

    Create 11899.cpp

---
+++ BOJ/11899.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string str;
+	cin >> str;
+	stack <char> s;
+	for (int i = 0; i < str.length(); i++) {
+		if (s.empty()) s.push(str[i]);
+		else if (str[i] == '(') {
+			s.push(str[i]);
+		}
+		else {
+			if (s.top() == '(') {
+				s.pop();
+			}
+			else {
+				s.push(str[i]);
+			}
+		}
+	}
+	cout << s.size() << '\n';
+	return 0;
+}

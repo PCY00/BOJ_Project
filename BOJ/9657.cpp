932f49565e4bbf6fb3fde9d585d26b2a2eec6593
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-03-13 19:24:20 +0900

    Create 9657.cpp

---
+++ BOJ/9657.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int dp[10001];
+
+int main() {
+	int n;
+	cin >> n;
+
+	dp[0] = 0;
+	dp[1] = 1;
+	dp[2] = 0;
+	dp[3] = 1;
+	dp[4] = 1;
+
+	for (int i = 5; i <= n; i++) {
+		if (dp[i - 1] == 0 || dp[i - 3] == 0 || dp[i - 4] == 0) {
+			dp[i] = 1;
+		}
+		else {
+			dp[i] = 0;
+		}
+	}
+	if (dp[n] == 1) {
+		cout << "SK" << '\n';
+	}
+	else {
+		cout << "CY" << '\n';
+	}
+	return 0;
+}

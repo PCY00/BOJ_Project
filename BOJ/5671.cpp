68bf4eb25c43a99b12d3ff8a7d19acbcd5edf3ef
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-03-14 19:08:51 +0900

    Create 5671.cpp

---
+++ BOJ/5671.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int n, m;
+	int ck1, ck2; //ck1은 숫자 범위, ck2는 중복숫자 검출
+	while (cin >> n >> m) {
+		ck1 = 0, ck2 = 0;
+		for (int i = n; i <= m; i++) {
+			ck1++;
+			int ck = 0;
+			int arr[10] = { 0, };
+			string str = to_string(i);
+			for (int j = 0; j < str.length(); j++) {
+				ck = str[j] - '0';
+				arr[ck]++;
+			}
+			for (int j = 0; j < 10; j++) {
+				if (arr[j] >= 2) {
+					ck2 += 1;
+					break;
+				}
+			}
+		}
+		cout << ck1 - ck2 << '\n';
+	}
+	return 0;
+}

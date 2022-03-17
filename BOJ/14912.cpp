799f0c8b7672b65c10b5bc371caf0e7c364116dd
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-03-17 11:36:52 +0900

    .
    .

---
+++ BOJ/14912.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n, d;
+	int ck = 0;
+	cin >> n >> d;
+	for (int i = 1; i <= n; i++) {
+		string str = to_string(i);
+		for (int j = 0; j < str.length(); j++) {
+			if (str[j] - '0' == d) {
+				ck++;
+			}
+		}
+	}
+	cout << ck << '\n';
+	return 0;
+}
---
+++ BOJ/2847.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int ck[101] = { 0, };
+
+int main() {
+	int Case;
+	cin >> Case;
+	int cnt = 0;
+	for (int i = 0; i < Case; i++) {
+		cin >> ck[i];
+	}
+	for (int j = Case - 1; j >= 0; j--) {
+		while (ck[j] <= ck[j - 1]) {            // 하나씩 줄여가면서 ck[j]보다 하나 작을때 까지 cnt++ 해주면서 실행
+			ck[j - 1]--;
+			cnt++;
+		}
+	}
+	cout << cnt << '\n';
+	return 0;
+}

3bd3820d9cf9acdb52f388a6ef1a5108b899a891
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-03-22 22:19:00 +0900

    Create 11478.cpp

---
+++ BOJ/11478.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include <set>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	set <string> set;
+	string str;
+	cin >> str;
+
+	string save = "";
+	for (int i = 0; i < str.length(); i++) {
+		for (int j = i; j < str.length(); j++) {
+			save += str[j];
+			set.insert(save);
+		}
+		save = "";
+	}
+	cout << set.size() << '\n';
+	return 0;
+}

ca92e9c6cd5239bad737b4f4f3717fd6e960f681
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-04-05 10:45:04 +0900

    Create 15652.cpp

---
+++ BOJ/15652.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int n, m;
+int arr[9] = { 0, };
+bool ck[9] = { false, };
+
+void dfs(int index, int cnt);
+
+int main() {
+	cin >> n >> m;
+	dfs(1, 0);
+	return 0;
+}
+
+void dfs(int index, int cnt) {
+	if (cnt == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	for (int i = index; i <= n; i++) {
+		ck[i] = true;
+		arr[cnt] = i;
+		dfs(i, cnt + 1);
+		ck[i] = false;
+	}
+}

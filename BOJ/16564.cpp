6d1d35d9042695c2ffcf2d74b2a3879ca9d9fed9
PCY00 <46348321+PCY00@users.noreply.github.com>
2022-04-09 18:57:19 +0900

    Create 16564.cpp

---
+++ BOJ/16564.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void dfs( int cnt);
+
+int n, m;
+int arr[10] = { 0, };
+int save[10] = { 0, };
+bool ck[10] = { false, };
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> save[i];
+	}
+	sort(save, save + n);
+	dfs(0);
+	return 0;
+}
+
+void dfs(int cnt) {
+	if (cnt == m) {
+		for (int i = 0; i < m; i++) {
+			cout << save[arr[i]] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	for (int i = 0; i < n; i++) {
+		if (!ck[i]) {
+			ck[i] = true;
+			arr[cnt] = i;
+			dfs(cnt + 1);
+			ck[i] = false;
+		}
+	}
+}

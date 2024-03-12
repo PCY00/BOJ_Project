dcb7ec4d6978ab861329c6489888ba057a947a11
PCY00 <46348321+PCY00@users.noreply.github.com>
2024-03-12 12:33:43 +0900

    Update 10998.c

--- BOJ_Other_Try/10998.c
+++ BOJ_Other_Try/10998.c
@@ -1,4 +1,3 @@
-/*
 #include <stdio.h>
 
 int Multiply(int multiplier, int multiplicand);
@@ -13,36 +12,15 @@ int main() {
 
 // multiplier : 곱할 숫자 , multiplicand : 곱해질 숫자 
 int Multiply(int multiplier, int multiplicand) {
-	int product;
+	int product = 0;
 
 	while (multiplicand != 0) {
 		if (multiplicand & 1) {
 			product += multiplier;
 		}
-		multiplier << 1;
-		multiplicand >> 1;
+		multiplier <<= 1;
+		multiplicand >>= 1;
 	}
 
 	return product;
-}
-*/
-#include <stdio.h>
-
-int multiply(int a, int b) {
-	int result = 0;
-	while (b != 0) {
-		if (b & 1) {
-			result += a;
-		}
-		a <<= 1;
-		b >>= 1;
-	}
-	return result;
-}
-
-int main() {
-	int a, b;
-	scanf("%d %d", &a, &b);
-	printf("%d", multiply(a, b));
-	return 0;
 }

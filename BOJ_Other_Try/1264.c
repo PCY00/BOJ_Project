f70b225e5d9fbd3f790cd6540dbb044815f563ab
PCY00 <46348321+PCY00@users.noreply.github.com>
2024-07-31 23:58:07 +0900

    Create 1264.c

---
+++ BOJ_Other_Try/1264.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[255];
+    int cnt = 0;
+    
+    while(1){
+        fgets (str, 255, stdin);
+        
+        for(int j = 0; str[j] != '\0'; j++){
+            if(str[j] == 'a' || str[j] == 'e' || str[j] == 'i' || str[j] == 'o' || str[j] == 'u' || str[j] == 'A' || str[j] == 'E' || str[j] == 'I' || str[j] == 'O' || str[j] == 'U'){
+                cnt++;
+            }
+        }
+        
+        if(str[0] == '#'){
+            break;
+        }
+        
+        printf("%d\n", cnt);
+        cnt = 0;
+        // 'a', 'e', 'i', 'o', 'u'
+        
+    }
+
+    return 0;
+}

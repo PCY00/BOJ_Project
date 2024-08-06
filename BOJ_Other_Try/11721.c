cd4188539ce9bb3f16e5ced09a8a789650468b8c
PCY00 <46348321+PCY00@users.noreply.github.com>
2024-08-06 23:52:42 +0900

    Create 11721.c

---
+++ BOJ_Other_Try/11721.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+
+    char str[100];
+
+    fgets(str, 100, stdin);
+
+    // 15 hello x3
+    for (int i = 1; i <= strlen(str); i++) {
+        printf("%c", str[i - 1]);
+        if (i % 10 == 0) {
+            printf("\n");
+        }
+    }
+}
